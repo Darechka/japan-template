66147008142423706c82c09a575fc0b92b55121a
Darya Zharskaya <darechka87@gmail.com>
2022-07-20 12:38:00 +0300

    feat: implement responsive design

--- js/main.js
+++ js/main.js
@@ -7,4 +7,18 @@
             header.classList.remove('header_active');
         }
     }
-}())
+}());
+
+(function() {
+    const burger = document.querySelector('.burger');
+    const navbar = document.querySelector('.header__nav');
+    const navbarClose = document.querySelector('.header__nav-close');
+
+    burger.addEventListener('click', () => {
+        navbar.classList.add('header__nav--active');
+    });
+
+    navbarClose.addEventListener('click', () => {
+        navbar.classList.remove('header__nav--active');
+    })
+}());
